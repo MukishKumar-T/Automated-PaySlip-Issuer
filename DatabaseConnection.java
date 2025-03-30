978545693b955301dd3295bfd9c4e79aa0b730e8
MukishKumar-T <152012098+MukishKumar-T@users.noreply.github.com>
2025-03-30 10:10:20 +0530

    Add files via upload
    Connecting DB to Java Project

---
+++ DatabaseConnection.java
@@ -0,0 +1,16 @@
+package dbconnection;
+
+import exception.DatabaseException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/employeepayslip";
+    private static final String username = "root";
+    private static final String pass = "root123";
+
+    public static Connection getConnection() throws SQLException {
+    	return DriverManager.getConnection(url, username, pass);
+    }
+}
