9693175143584a8790850c9987026acee2202f98
MukishKumar-T <152012098+MukishKumar-T@users.noreply.github.com>
2025-03-30 10:11:13 +0530

    Updated DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String url = "jdbc:mysql://localhost:3306/employeepayslip";
     private static final String username = "root";
-    private static final String pass = "root123";
+    private static final String pass = "******";
 
     public static Connection getConnection() throws SQLException {
     	return DriverManager.getConnection(url, username, pass);
